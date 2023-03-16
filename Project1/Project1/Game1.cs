1a9eb039e7f86bdfecc57ee1c791a7084b8fea7f
Filipe o Araujo <paulo.lipe.13@hotmail.com>
2023-03-16 16:50:20 +0000

    savepoint

--- Project1/Project1/Game1.cs
+++ Project1/Project1/Game1.cs
@@ -5,6 +5,11 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+public enum Direction
+{
+    Up, Down, Left, Right // 0, 1, 2, 3
+}
+
 namespace Project1
 {
     public class Game1 : Game
@@ -15,17 +20,28 @@ namespace Project1
         private int nrLinhas = 0;
         private int nrColunas = 0;
         public char[,] level;
-        private Texture2D player, dot, box, wall;
+        private Texture2D dot, box, wall; //Load images Texture
+        private Texture2D[] player;
         int tileSize = 64;
         private Player Sokoban;
         public List<Point> boxes;
+        public Direction direction = Direction.Down;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-        }
 
+
+        }
+        public bool Victory()
+        {
+            foreach (Point b in boxes) // pecorrer a lista das caixas
+            {
+                if (level[b.X, b.Y] != '.') return false; // verifica se há caixas sem pontos
+            }
+            return true;
+        }
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -41,11 +57,15 @@ namespace Project1
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("File");
-            player = Content.Load<Texture2D>("Character4");
             dot = Content.Load<Texture2D>("EndPoint_Yellow");
             box = Content.Load<Texture2D>("Crate_Gray");
             wall = Content.Load<Texture2D>("WallRound_Black");
             // TODO: use this.Content to load your game content here
+            player = new Texture2D[4];
+            player[(int)Direction.Down] = Content.Load<Texture2D>("Character4");
+            player[(int)Direction.Up] = Content.Load<Texture2D>("Character7");
+            player[(int)Direction.Left] = Content.Load<Texture2D>("Character1");
+            player[(int)Direction.Right] = Content.Load<Texture2D>("Character2");
         }
 
         protected override void Update(GameTime gameTime)
@@ -54,6 +74,10 @@ namespace Project1
                 Exit();
 
             // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.R)) Initialize();
+
+            if (Victory()) Exit();
+
             Sokoban.Update(gameTime);
 
             base.Update(gameTime);
@@ -68,7 +92,7 @@ namespace Project1
             _spriteBatch.DrawString(font, "-No ceu tem pao?", new Vector2(50, 20), Color.PaleVioletRed);
             _spriteBatch.DrawString(font, $"Numero de Linhas = {nrLinhas} \n Numero de Colunas = {nrColunas}", new Vector2(0, 50), Color.Black);
             Rectangle position = new Rectangle(0, 0, tileSize, tileSize);
-
+            
 
 
             for (int x = 0; x < level.GetLength(0); x++) //pega a primeira dimensão
@@ -91,12 +115,13 @@ namespace Project1
                         case 'X':
                             _spriteBatch.Draw(wall, position, Color.White);
                             break;
+
                     }
                 }
             }
             position.X = Sokoban.Position.X * tileSize; //posição do Player
             position.Y = Sokoban.Position.Y * tileSize; //posição do Player
-            _spriteBatch.Draw(player, position, Color.White); //desenha o Player
+            _spriteBatch.Draw(player[(int)direction], position, Color.White); //desenha o Player
 
             foreach (Point b in boxes)
             {
